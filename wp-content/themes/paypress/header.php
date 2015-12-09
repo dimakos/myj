3fa0290f0588703051f99ce6abbbfc8b564a9ca2
dimakos <koss_man@mail.ru>
2015-12-09 14:58:10 +0200

    2 comm

---
+++ wp-content/themes/paypress/header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Шаблон шапки (header.php)
+ * @package WordPress
+ * @subpackage your-clean-template
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); // вывод атрибутов языка ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); // кодировка ?>">
+	<?php /* RSS и всякое */ ?>
+	<link rel="alternate" type="application/rdf+xml" title="RDF mapping" href="<?php bloginfo('rdf_url'); ?>">
+	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
+	<link rel="alternate" type="application/rss+xml" title="Comments RSS" href="<?php bloginfo('comments_rss2_url'); ?>">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/style.css">
+	<link rel="stylesheet" type="text/css" href="http://sources.sites.org.ua/bootstrap/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/font-awesome/css/font-awesome.css" /> 
+	 <!--[if lt IE 9]>
+	 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	 <![endif]-->
+	<title><?php typical_title(); // выводи тайтл, функция лежит в function.php ?></title>
+	<?php wp_head(); // необходимо для работы плагинов и функционала ?>
+</head>
+<body <?php body_class(); // все классы для body ?>>
+	<header class="dfgh">
+		<?php $args = array( // опции для вывода верхнего меню, чтобы они работали, меню должно быть создано в админке
+			'theme_location' => 'top', // идентификатор меню, определен в register_nav_menus() в function.php
+			'container'=> 'nav', // обертка списка
+			'menu_class' => 'bottom-menu', // класс для ul
+	  		'menu_id' => 'bottom-nav', // id для ul
+  			);
+			wp_nav_menu($args); // выводим верхнее меню
+			if ( is_user_logged_in() ) { ?>
+				<script>jQuery("#bottom-nav").append('<li id="menu-item-profile" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-profile"><a href="http://paypress.sites.org.ua/profile/">Profile</a></li>');</script>
+			<?php }
+		?>
+	</header>
